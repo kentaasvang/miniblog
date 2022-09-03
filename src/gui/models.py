44878ee54b6177468743b289ff47878bc975d1f9
Kent Martin Åsvang <kentaasvang@gmail.com>
2022-09-03 12:29:16 +0200

    added model for posts with tests

--- src/gui/models.py
+++ src/gui/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField(null=True)
+    is_published = models.BooleanField(default=False)
+    created = models.DateTimeField(editable=False)
+
+    def save(self, *args, **kwargs) -> None:
+        """
+        Update timestamps  
+        """
+        if _first_save(self):
+            self.created = timezone.now()
+        
+        return super(Post, self).save(*args, **kwargs) 
+            
+
+def _first_save(post: Post) -> bool:
+    return True if not post.id else False
--- src/gui/tests.py
+++ src/gui/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from gui.models import Post
 
-# Create your tests here.
+
+class PostTests(TestCase):
+
+    TITLE="my title"
+    BODY="this is my content"
+    PUBLISHED=False
+
+    def setUp(self) -> None:
+        Post.objects.create(
+            title=self.TITLE,
+            body=self.BODY,
+            is_published=self.PUBLISHED,
+            )
+
+    def test_get(self):
+        post: Post = Post.objects.get(id=1)
+        self.assertEqual(post.title, self.TITLE)
+        self.assertEqual(post.body, self.BODY)
+        self.assertEqual(post.is_published, self.PUBLISHED)
