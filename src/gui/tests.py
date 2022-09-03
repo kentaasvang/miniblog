d07acbd6486bdf9306bdb1399ba781c4feedd37f
Kent Martin Åsvang <kentaasvang@gmail.com>
2022-09-03 15:29:17 +0200

    installed Faker, updated tests and we are not showing unpublished posts in frontview

--- src/gui/tests.py
+++ src/gui/tests.py
@@ -3,31 +3,66 @@ from django.test import (
     Client
 )
 
+from faker import Faker
+
 from .models import Post
+
 
 
 class PostTests(TestCase):
 
-    TITLE="my title"
-    BODY="this is my content"
-    PUBLISHED=False
+    faker = Faker()
+
+    pub_posts = []
+    not_pub_posts = []
+
+    for _ in range(10):
+        pub_posts.append(
+           Post(
+               title=faker.text(10),
+               body=faker.text(),
+               is_published=True
+           ) 
+        )
+
+    for _ in range(5):
+        not_pub_posts.append(
+           Post(
+               title=faker.text(10),
+               body=faker.text(),
+               is_published=False
+           ) 
+        )
+
+    posts = pub_posts + not_pub_posts
 
     def setUp(self):
         self.client = Client()
 
-        Post.objects.create(
-            title=self.TITLE,
-            body=self.BODY,
-            is_published=self.PUBLISHED,
-            )
+        for post in self.posts:
+            Post.objects.create(
+                title=post.title,
+                body=post.body,
+                is_published=post.is_published,
+                )
 
     def test_simple_db_get(self):
-        post: Post = Post.objects.get(id=1)
-        self.assertEqual(post.title, self.TITLE)
-        self.assertEqual(post.body, self.BODY)
-        self.assertEqual(post.is_published, self.PUBLISHED)
+        posts = Post.objects.all()
+        self.assertEqual(len(posts), len(self.posts))
 
-    def test_frontpage_displays_posts_title(self):
+    def test_gui_index_view_shows_all_published_posts(self):
         response = self.client.get("/")
 
-        self.assertContains(response, self.TITLE)
+        for post in self.pub_posts:
+            self.assertContains(response, post.title)
+
+        self.assertEqual(
+            len(response.context["posts"]), 
+            len(self.pub_posts)
+            )
+
+    def test_gui_index_view_doesnt_show_unpublished_posts(self):
+        response = self.client.get("/")
+
+        for post in self.not_pub_posts:
+            self.assertNotContains(response, post.title)
--- src/gui/views.py
+++ src/gui/views.py
@@ -7,6 +7,6 @@ from .models import Post
 
 
 def index(req: HttpRequest) -> HttpResponse:
-    posts = Post.objects.all()
+    posts = Post.objects.filter(is_published=True)
     context = {"posts": posts}
     return render(req, "gui/index.html", context=context)
