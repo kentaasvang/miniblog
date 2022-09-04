84e987d29d138c34f1c032d78d58c6e4d6a92eca
Kent Martin Åsvang <kentaasvang@gmail.com>
2022-09-04 10:42:21 +0200

    updated code to publish post on post.html by id

--- src/gui/views.py
+++ src/gui/views.py
@@ -6,7 +6,13 @@ from django.http import (
 from .models import Post
 
 
-def index(req: HttpRequest) -> HttpResponse:
+def index(req):
     posts = Post.objects.filter(is_published=True)
     context = {"posts": posts}
     return render(req, "gui/index.html", context=context)
+
+
+def post(req, id):
+    post = Post.objects.get(id=id) 
+    context = {"post": post}
+    return render(req, "gui/post.html", context=context)
